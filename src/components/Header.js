19f6541379c888101b09da74c9c5008600c95cf4
masbagaspn <masbagaspn@gmail.com>
2022-12-16 12:50:36 +0700

    feat: fontawesome

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navigation from './Navigation'
 
 function Header() {
   return (
-    <header className='text-lg text-red-500'>
-        Header
+    <header className='p-4 flex space-between items-center border-b-2'>
+        <span className='font-bold'>Rakamin</span>
+        <Navigation />
     </header>
   )
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function Navigation() {
+  return (
+    <nav>
+
+    </nav>
+  )
+}
+
+export default Navigation
